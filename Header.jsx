7ee7498278c1ed8595183e61da4758e82f8f85e7
Vanshika466 <vanshika4823.be23@chitkara.edu.in>
2024-11-04 15:54:20 +0530

    Add files via upload

--- Header.jsx
+++ Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Header as SemanticHeader } from 'semantic-ui-react'; // Ensure this is here
+import { Header as SemanticHeader } from 'semantic-ui-react'; 
 
 const Header = () => (
   <SemanticHeader as="h1" textAlign="center">
